a8bcfbe363e01f3fe173fe1d7411f855ab291cf4
Michael Klimek <michaelklimek@g.harvard.edu>
2014-09-16 21:54:33 -0500

    Added index.php, photo and CSS

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Michael Klimek</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="general">
+		<div class="name">
+			<h1>Michael Klimek</h1>
+			<img src="mike.jpg" width="200" length="200" id="photo">
+		</div>
+	</div>
+	<div class="project">
+		<div id="p2">
+			<h3>Project 2</h3>
+			<a href="#">URL</a>
+			<a href="#">GitHub</a>
+		</div>
+		<div id="p3">
+			<h3>Project 3</h3>
+			<a href="#">URL</a>
+			<a href="#">GitHub</a>
+		</div>
+		<div id="p4">
+			<h3>Project 4</h3>
+			<a href="#">URL</a>
+			<a href="#">GitHub</a>
+		</div>
+	</div>
+	<div class="about">
+		<h2>About Me</h2>
+		<div>
+			<h3>Who I am and What I do</h3>
+			<?php // 2+ sentences ?>
+			<p>President and co-founder of the Pillar Corporation, an independent insurance brokerage based out of Illinois. I am licensed in Property, Casualty, Life and Health Insurance in 12 different states. My primary focus is on commercial insurance for businesses in certain niche industries.</p>
+		</div>
+		<div>
+			<h3>Why I am taking CSCI E-15</h3>
+			<p>I see the potential for increased production and efficiency in my company by developing and utilizing dynamic applications. I am taking this course in hopes to develop a strong foundation in PHP and database programming.</p>
+		</div>
+		<div>
+			<h3>My previous experience with programming</h3>
+			<p>I have had very little formal training in programming. My experience has been limited to self study by reading books, watching youtube videos, auditing edx classes and reviewing some teamtreehouse material. I have been programming since the beginning of 2013.</p>
+		</div>
+		<div>
+			<h3>What is my operation platform?</h3>
+			<p>My current operating platform is Windows based.</p>
+		</div>
+		<div>
+			<h3>Additional Information</h3>
+			<p>I am excited to be taking this course and plan to continue my graduate studies in the Information Technology program.</p>
+		</div>
+	</div>
+
+</body>
+<footer>
+	<p>&copy; <?php echo date('Y'); ?> Michael Klimek. All Rights Reserved.</p>
+</footer>
+</html>
